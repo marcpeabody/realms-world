f081341748d5ba437b53f5f8f4211bf8e1a7e77b
Tony Schneider <tonywok@gmail.com>
2017-05-07 10:23:36 -0400

    create players, games and decisions
    This should be the minimal required for simulation

--- db/migrate/20170310040730_create_games.rb
+++ db/migrate/20170310040730_create_games.rb
@@ -1,7 +1,23 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
-    create_table :games do |t|
+    create_table :players, id: :bigserial do |t|
+      t.string :username
+
       t.timestamps
+    end
+
+    create_table :games, id: :bigserial do |t|
+      t.string :seed
+      t.bigint :player_one_id, null: false
+      t.bigint :player_two_id, null: false
+
+      t.timestamps
+    end
+
+    create_table :decisions, id: :bigserial do |t|
+      t.string :key
+
+      t.bigint :game_id, null: false
     end
   end
 end
