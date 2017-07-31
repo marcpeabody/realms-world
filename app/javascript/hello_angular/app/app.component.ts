fe055bf622dbb4bb3262212b2a5c8ffb54064e0a
Tony Schneider <tonywok@gmail.com>
2017-07-31 01:29:48 -0400

    setup angular 2 using web packer gem

---
+++ app/javascript/hello_angular/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import templateString from './template.html'
+
+@Component({
+  selector: 'hello-angular',
+  template: templateString
+})
+
+export class AppComponent {
+  name = 'Angular!'
+}
